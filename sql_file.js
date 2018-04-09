ae95db007434faafe4a823951874224647bcc2ce
dave108 <kchauvo@masonlive.gmu.edu>
2018-04-09 19:50:10 -0400

    Added a table row

--- sql_file.js
+++ sql_file.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
+  host: "localhost",
+  user: "root",
+  password: "JoeHadit2018",
    database: "usersdb",
     timezone: 'utc'
 });
@@ -18,8 +18,8 @@ con.connect(function(err) {
 	  if(err) throw err;
 	  console.log("Database usersdb created");
   });
-  // max discord limit is 32 chars, 35 for extra safety
-  var sql = "CREATE TABLE users (name VARCHAR(35) not null, login DATETIME(0), credit INT not null, strike INT not null)";
+  // max discord limit is 32 chars, 35 for extra safety, id is 25 for safety purpose
+  var sql = "CREATE TABLE users (name VARCHAR(35) not null, login DATETIME(0), credit INT not null, strike INT not null, id VARCHAR(20) not null)";
   con.query(sql, function(err,result){
 	  if(err) throw err;
 	  console.log("Table created");
