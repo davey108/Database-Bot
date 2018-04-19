2b3303c8c914f2a3000b20db0442f4a4b554c8d6
dave108 <32458148+davey108@users.noreply.github.com>
2018-04-18 23:12:20 -0400

    Added feature to write end of log
    -Write to previous date that a new date is starting and that day log is finished

--- text_reader.js
+++ text_reader.js
@@ -3,20 +3,24 @@ let date = new Date();
 let path = './' + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + '.txt';
 // if the file does not exist
 if(!fs.existsSync(path)){
-	console.log(path);
+	// maybe we want to end previous file log too?
+	let prevD = new Date(date);
+	prevD.setDate(date.getDate()-1);
+	let prevP = './' + prevD.getFullYear() + "-" + (prevD.getMonth() + 1) + "-" + prevD.getDate() + '.txt';
+	console.log(prevP);
+	if(fs.existsSync(prevP)){
+		let prevStream = fs.createWriteStream(prevP, {'flags' : 'a'});
+		prevStream.end('This is the end of today log');
+	}	
 	// create file
 	fs.closeSync(fs.openSync(path, 'w'));
 }
 let logStream = fs.createWriteStream(path, {'flags': 'a'});
 
 
-
-
-
-
 function writeContent(eventype, message){
-	
-	
+	let date = new Date();
+	logStream.write(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + eventype + "-" + message);	
 }
 
 
